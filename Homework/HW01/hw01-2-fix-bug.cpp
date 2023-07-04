d73d5cf68b131c9098986d611052af225b0a0292
Thanarat420 <taexxx4@gmal.com>
2023-07-04 21:07:33 +0700

    hw2

--- Homework/HW01/hw01-2-fix-bug.cpp
+++ Homework/HW01/hw01-2-fix-bug.cpp
@@ -1,8 +1,8 @@
 //TODO: #6 Fix all bugs.
 #include <stdio.h>
 int main() {
-    printf( "Hello, I'm Javis (RMUTL-Version)" ) ;
-    printf( "This is my first time to coding with C language" ) ;
-    printf( "Today is my birthday" ) ;
+    printf( "Hello, I'm Javis (RMUTL-Version).\n" ) ;
+    printf( "This is my first time to coding with C language.\n" ) ;
+    printf( "Today is my birthday.\n" ) ;
     return 0 ;
 }
